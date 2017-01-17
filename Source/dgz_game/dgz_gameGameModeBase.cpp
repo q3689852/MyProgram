338d128a9d39549ebf5fca19f166ec0cf460f7eb
q3689852 <3689852@qq.com>
2017-01-17 14:48:32 +0800

    Initial commit

---
+++ Source/dgz_game/dgz_gameGameModeBase.cpp
@@ -0,0 +1,8 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "dgz_game.h"
+#include "dgz_gameGameModeBase.h"
+
+
+
+
---
+++ Source/dgz_game/dgz_gameGameModeBase.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameModeBase.h"
+#include "dgz_gameGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DGZ_GAME_API Adgz_gameGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
