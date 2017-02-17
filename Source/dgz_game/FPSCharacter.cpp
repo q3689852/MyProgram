bb6af3dd68e055c7d753f05dd28a02133b986b11
q3689852 <3689852@qq.com>
2017-02-17 14:21:41 +0800

    red_font

---
+++ Source/dgz_game/FPSCharacter.cpp
@@ -0,0 +1,39 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "dgz_game.h"
+#include "FPSCharacter.h"
+
+
+// Sets default values
+AFPSCharacter::AFPSCharacter()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AFPSCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	if (GEngine)
+	{
+		// 显示调试信息五秒。-1“键”值（首个参数）说明我们无需更新或刷新此消息。
+		GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Red, TEXT("We are using FPSCharacter."));
+	}
+}
+
+// Called every frame
+void AFPSCharacter::Tick( float DeltaTime )
+{
+	Super::Tick( DeltaTime );
+
+}
+
+// Called to bind functionality to input
+void AFPSCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
